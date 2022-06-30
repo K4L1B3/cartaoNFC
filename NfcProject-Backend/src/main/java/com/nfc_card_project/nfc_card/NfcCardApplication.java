a184383405bc5b21389a94762e609083365c6771
K4L1B3 <luizhlimagomes28@gmail.com>
2022-06-30 15:52:57 -0300

    atualizando repositorio com versão definitiva para a criação

---
+++ NfcProject-Backend/src/main/java/com/nfc_card_project/nfc_card/NfcCardApplication.java
@@ -0,0 +1,13 @@
+package com.nfc_card_project.nfc_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfcCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfcCardApplication.class, args);
+	}
+
+}
